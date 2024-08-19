ec9c2df273efb9b38a89f82e93a170d3df5588f5
Brighten Zhang <bistromath25@gmail.com>
2024-08-19 10:21:17 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import StoreProvider from '../app/StoreProvider';
+import StoreProvider from './store/StoreProvider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
