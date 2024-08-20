482e9ab332f2bbc1cc41bb78d9441f3aab6e2b2f
Brighten Zhang <bistromath25@gmail.com>
2024-08-19 20:58:04 -0400

    cleanup

--- src/app/utils/randInt.ts
+++ src/app/utils/randInt.ts
@@ -2,12 +2,16 @@ export const randInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+export const randItem = (items: any[]) => {
+  return items[randInt(0, items.length - 1)];
+};
+
 // https://stackoverflow.com/a/19270021
 export const randInts = (max: number, n: number) => {
   var result = new Array(n),
     taken = new Array(max);
   while (n--) {
-    const r = Math.floor(Math.random() * max);
+    const r = randInt(0, max - 1);
     result[n] = r in taken ? taken[r] : r;
     taken[r] = --max in taken ? taken[max] : max;
   }
